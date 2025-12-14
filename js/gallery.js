39634c0f166c693ced94888e5ddfb05811c3160b
Taras <tarassakin@gmail.com>
2025-12-14 07:38:41 -0800

    DZ-8-4

---
+++ js/gallery.js
@@ -0,0 +1,60 @@
+{
+  /* <li class="gallery-item">
+  <a
+    class="gallery-link"
+    href="https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg"
+  >
+    <img
+      class="gallery-image"
+      src="https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__340.jpg"
+      data-source="https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg"
+      alt="Hokkaido Flower"
+    />
+  </a>
+</li>; */
+}
+{
+  /* <li class="gallery-item">
+  <a
+    class="gallery-link"
+    href="https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpglarge-image.jpg"
+  >
+    <img
+      class="gallery-image"
+      src="https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg"
+      data-source="https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg"
+      alt="Container Haulage FreightImage description"
+    />
+  </a>
+</li>; */
+}
+{
+  /* <li class="gallery-item">
+  <a class="gallery-link" href="https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg">
+    <img
+      class="gallery-image"
+      src="https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg"
+      data-source="https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg"
+      alt="Aerial Beach View"
+    />
+  </a>
+</li>; */
+}
+
+// В атрибуті src тега <img> вказуємо посилання на маленьку версію зображення.
+// Для атрибута alt використовуємо опис зображення.
+// Посилання на велике зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання.
+// Зверни увагу на те, що зображення огорнуте посиланням, у якого атрибут href вказує на шлях до файлу з зображенням. Отже клік по ньому може викликати завантаження зображення на комп’ютер користувача. Заборони цю поведінку за замовчуванням.
+// ------------------------------------------------------------------
+{
+  /* <li class="gallery-item">
+  <a class="gallery-link" href="large-image.jpg">
+    <img
+      class="gallery-image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</li>; */
+}
--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const nameInput = document.querySelector('#name-input');
-
-const nameOutput = document.querySelector('#name-output');
-
-nameInput.addEventListener('input', () => {
-  let name = nameInput.value.trim();
-  nameOutput.textContent = name ? name : 'Anonymous';
-});
